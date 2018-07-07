9b170ea7e982fe14f5cd1339093d67763f0168cb
Shashank Mohabia <shashankmohabia27@gmail.com>
2018-07-07 23:22:00 +0530

    Toolbar items added in blog detail activity

--- app/src/main/java/com/example/shashankmohabia/clinmd/Core/Home/Blog/BlogDetailView/BlogDetailActivity.kt
+++ app/src/main/java/com/example/shashankmohabia/clinmd/Core/Home/Blog/BlogDetailView/BlogDetailActivity.kt
@@ -3,8 +3,11 @@ package com.example.shashankmohabia.clinmd.Core.Home.Blog.BlogDetailView
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import com.example.shashankmohabia.clinmd.R
 import kotlinx.android.synthetic.main.activity_blog_detail.*
+import org.jetbrains.anko.toast
 
 class BlogDetailActivity : AppCompatActivity() {
 
@@ -13,8 +16,22 @@ class BlogDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_blog_detail)
         setSupportActionBar(toolbar)
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Bookmark this blog", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_blog_detail, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.share -> {
+                toast("Share this Blog")
+            }
+        }
+        return true
     }
 }
